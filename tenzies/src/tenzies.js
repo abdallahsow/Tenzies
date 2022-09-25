0edc296e3eaf0ed6b92bdce5c7a79393bf87535f
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-25 14:24:08 +0300

    set up the layout of the app

---
+++ tenzies/src/tenzies.js
@@ -0,0 +1,13 @@
+import "./tenzies.css";
+
+const App = function () {
+    return <main className="app">
+        <div className="container">
+            <div className="game">
+                <h1 className="game-title">Tenzies</h1>
+            </div>
+      </div>
+  </main>;
+};
+
+export default App;
