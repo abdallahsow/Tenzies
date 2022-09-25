0fbb67cdf03383e1792754808573c44dbccccb2c
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-25 14:43:01 +0300

    created the Header component

---
+++ tenzies/src/components/Dice.js
@@ -0,0 +1,5 @@
+const Dice = function () {
+    return <div></div>;
+};
+
+export default Dice;
--- tenzies/src/tenzies.js
+++ tenzies/src/tenzies.js
@@ -1,13 +1,16 @@
 import "./tenzies.css";
+import Header from "./components/Header";
 
 const App = function () {
-    return <main className="app">
-        <div className="container">
-            <div className="game">
-                <h1 className="game-title">Tenzies</h1>
-            </div>
+  return (
+    <main className="app">
+      <div className="container">
+        <div className="game">
+          <Header />
+        </div>
       </div>
-  </main>;
+    </main>
+  );
 };
 
 export default App;
