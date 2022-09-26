f16e35b2510c3079a5e47d620dc4e3116bf90515
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-26 14:22:09 +0300

    updated the css of the dices

--- tenzies/src/components/Dice.js
+++ tenzies/src/components/Dice.js
@@ -1,9 +1,11 @@
 const Dice = function () {
-  return (
-    <div className="dice-box">
-      <p className="dice">5</p>
+  const dices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const renderedDices = dices.map((dice) => (
+    <div key={dice} className="dice-box">
+      <p className="dice">{dice}</p>
     </div>
-  );
+  ));
+  return <div className="grid-layout">{renderedDices}</div>;
 };
 
 export default Dice;
