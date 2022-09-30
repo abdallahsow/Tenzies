4315f5f71b52fca2147b22b2f03a14d09d0054de
Abdallah Sow <abdallahsow700@gmail.com>
2022-09-30 14:05:06 +0300

    created state to hold our array of numbers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Dice from "./components/Dice";
 import "./App.css";
 
 const App = function () {
+  const allNewDice = () => {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  };
+
+  const [dices, setDices] = useState(allNewDice());
+
   return (
     <main className="app">
       <div className="container">
         <div className="game">
           <Header />
-          <Dice />
+          <div className="grid-layout">
+            {dices.map((dice, index) => (
+              <Dice key={index} dice={dice} />
+            ))}
+          </div>
           <button className="roll-dice">Roll</button>
         </div>
       </div>
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,11 +1,9 @@
-const Dice = function () {
-  const dices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const renderedDices = dices.map((dice) => (
+const Dice = function ({ dice }) {
+  return (
     <div key={dice} className="dice-box">
       <p className="dice">{dice}</p>
     </div>
-  ));
-  return <div className="grid-layout">{renderedDices}</div>;
+  );
 };
 
 export default Dice;
