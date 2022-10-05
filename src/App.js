5dd4b01c802a25f58409bf147836be8d80db49f6
Abdallah Sow <abdallahsow700@gmail.com>
2022-10-05 14:09:00 +0300

    add more features to the app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,80 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 import Header from "./components/Header";
 import Dice from "./components/Dice";
 import "./App.css";
 
 const App = function () {
-  const allNewDice = () => {
+  const [dices, setDices] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dices.every((die) => die.isHeld);
+    const firstValue = dices[0].value;
+    const allSame = dices.every((die) => die.value === firstValue);
+    if (allHeld && allSame) {
+      setTenzies(true);
+      console.log("You won");
+    }
+  }, [dices]);
+
+  function generateRandomDice() {
+    return {
+      id: nanoid(),
+      isHeld: false,
+      value: Math.ceil(Math.random() * 6),
+    };
+  }
+
+  function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      newDice.push(generateRandomDice());
     }
     return newDice;
+  }
+
+  function rollDie() {
+    if (!tenzies) {
+      setDices(() => {
+        return dices.map((die) => (die.isHeld ? die : generateRandomDice()));
+      });
+    } else {
+      setTenzies(false);
+      setDices(allNewDice());
+    }
+  }
+
+  const hold = (id) => {
+    setDices(() => {
+      return dices.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      );
+    });
   };
 
-  const [dices, setDices] = useState(allNewDice());
+  const rollText = tenzies ? "New Game" : "Roll";
 
   return (
-    <main className="app">
-      <div className="container">
-        <div className="game">
-          <Header />
-          <div className="grid-layout">
-            {dices.map((dice, index) => (
-              <Dice key={index} dice={dice} />
-            ))}
+    <div>
+      {tenzies && <Confetti />}
+      <main className="app">
+        <div className="container">
+          <div className="game">
+            <Header />
+            <div className="grid-layout">
+              {dices.map((die) => (
+                <Dice key={die.id} {...die} handleClick={hold} />
+              ))}
+            </div>
+            <button className="roll-dice" onClick={rollDie}>
+              {rollText}
+            </button>
           </div>
-          <button className="roll-dice">Roll</button>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 };
 
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,7 +1,11 @@
-const Dice = function ({ dice }) {
+const Dice = function ({ value, handleClick, isHeld, id }) {
+  const styles = {
+    backgroundColor: isHeld ? "#59E391" : "#fff",
+  };
+  
   return (
-    <div key={dice} className="dice-box">
-      <p className="dice">{dice}</p>
+    <div className="dice-box" style={styles} onClick={() => handleClick(id)}>
+      <p className="dice">{value}</p>
     </div>
   );
 };
